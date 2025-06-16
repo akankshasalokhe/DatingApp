aefb39fd10dde349d6c16cf56c17481c3ec0f3d5
akankshasalokhe <akankshasalokhe2001@gmail.com>
2025-06-16 11:28:47 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
+// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error('DB connection failed', error);
-
-
+    console.error('MongoDB connection error:', error.message);
     process.exit(1);
   }
 };
